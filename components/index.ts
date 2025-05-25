52cdebc2382358cdd6129aa9632458186c7be63c
Isaac Yakubu <iamisaacyakubu3@gmail.com>
2025-05-25 20:42:27 +0100

    favicon error repairs

--- components/index.ts
+++ components/index.ts
@@ -14,6 +14,7 @@ import Works from "./sections/Works";
 import Feedbacks from "./sections/Feedbacks";
 import Contact from "./sections/Contact";
 import CanvasLoader from "./layout/Loader";
+import herobg from '../assets/herobg.png'; // adjust the path as needed
 
 export {
   Hero,
