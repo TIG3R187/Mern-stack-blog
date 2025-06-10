57cb6530e01622559e0c97f32952d1edf9be1bd9
Isaac Yakubu <iamisaacyakubu3@gmail.com>
2025-06-10 22:30:30 +0100

    corrected paths

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/Mern-stack-blog/',
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1200, // Increase warning limit
